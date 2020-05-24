55ecc6ccf44225d9e28133ff0be4162e0d545335
Aaron Chong <aaronchongth@gmail.com>
2020-05-25 02:18:49 +0800

    Started basic client prototype using NamedTuple classes instead of IDL

---
+++ client/Client.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+import time
+import itertools
+
+from zenoh.net import Session
+
---
+++ client/Messages.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import pickle
+from typing import NamedTuple, List
+
+
+class Time(NamedTuple):
+    sec: int
+    nanosec: int
+
+
+class RobotMode(NamedTuple):
+    mode: int
+    
+    MODE_IDLE = 0
+    MODE_CHARGING = 1
+    MODE_MOVING = 2
+    MODE_PAUSED = 3
+    MODE_WAITING = 4
+    MODE_EMERGENCY = 5
+    MODE_GOING_HOME = 6
+    MODE_DOCKING = 7
+
+
+class Location(NamedTuple):
+    t: Time
+    x: float
+    y: float
+    yaw: float
+    level_name: str
+
+
+class RobotState(NamedTuple):
+    name: str
+    model: str
+    task_id: str
+    mode: RobotMode
+    battery_percent: float
+    location: Location
+    path: List[Location]
+
+
+# if __name__ == '__main__':
+#     test_time = Time(sec=10, nanosec=12)
+#     test_location = Location(
+#         t=test_time, 
+#         x=123.123, 
+#         y=234.234, 
+#         yaw=345.345,
+#         level_name='test_location')
+#     test_mode = RobotMode(mode=RobotMode.MODE_IDLE)
+#     test_state = RobotState(
+#         name='test_robot',
+#         model='test_robot_model',
+#         task_id='test_robot_task_id',
+#         mode=test_mode,
+#         battery_percent=123.123,
+#         location=test_location,
+#         path=[test_location, test_location, test_location])
+
+#     serialized_state = pickle.dumps(test_state)
+#     print(len(serialized_state))
+#     deserialized_state = pickle.loads(serialized_state)
+#     print(deserialized_state)
